0bf7efc15cbbd39bf7b8f8fd63fb8b7abb69319b
chineseifc <97346630+chineseifc@users.noreply.github.com>
2023-04-21 21:43:50 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import Ido from './components/Ido.vue'
+import ConnectWallet from './components/ConnectWallet.vue'
+</script>
+
+<template>
+  <div class= "header">
+  <ConnectWallet />
+  </div>
+  <div>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/cxelogo.svg" class="logo vue" alt="CXE" />
+    </a>
+  </div>
+  <Ido msg="Develop EOS-EVM" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+.header{
+  top:10px;
+  position:absolute;
+  right:20px;
+}
+</style>
---
+++ src/components/ConnectWallet.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+</script>
+
+<template>
+  
+  <button class= "but0" scale= "sm" type="button" @click="get">
+    <div class= "but1" display= "none,,,block">连接钱包</div>
+    <div class= "but2" display= "block,,,none">连接</div>
+  </button>
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+.but0{
+  position: relative;
+    -webkit-box-align: center;
+    align-items: center;
+    border: 0px;
+    border-radius: 16px;
+    box-shadow: rgba(14, 14, 44, 0.4) 0px -1px 0px 0px inset;
+    cursor: pointer;
+    display: block;
+    font-family: inherit;
+    font-size: 16px;
+    font-weight: 600;
+    -webkit-box-pack: center;
+    justify-content: center;
+    letter-spacing: 0.03em;
+    line-height: 1;
+    opacity: 1;
+    outline: 0px;
+    transition: background-color 0.2s ease 0s, opacity 0.2s ease 0s;
+    height: 32px;
+    padding: 0px 16px;
+    background-color: var(--colors-primary);
+    color: var(--colors-invertedContrast);
+}
+.but1{
+  display: none;
+}
+.bu2{
+  display: block;
+}
+</style>
